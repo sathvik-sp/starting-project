2d464375244c9c14a50ad34988dd650944cce6c3
SathvikSP <sathviksp123@gmail.com>
2024-09-13 10:32:59 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from 'react';
-import {CORE_CONCEPTS} from './data.js';
+import { CORE_CONCEPTS } from './data.js';
 import Header from './Components/Header/Header.jsx';
 import CoreConcept from './Components/CoreConcept.jsx';
 import BtnComp from './Components/BtnComp.jsx';
-import {EXAMPLES} from './data.js'
+import { EXAMPLES } from './data.js'
 function App() {
 
-  const [selectedStateValue, setSelectedState]=useState("components")
+  const [selectedStateValue, setSelectedState] = useState("")
 
-  function onSelectHandler(selectedButton){
+  function onSelectHandler(selectedButton) {
     setSelectedState(selectedButton);
     //console.log(selectedButton);
   }
-  
+
+  let tabContent= <p>select any topic</p> ;
+
+  if (selectedStateValue){
+    tabContent=
+    (<div id="tab-content">
+    <h3>{EXAMPLES[selectedStateValue].title}</h3>
+    <p>{EXAMPLES[selectedStateValue].description}</p>
+    <code>{EXAMPLES[selectedStateValue].code}</code>
+  </div>);
+  }
+
   return (
     <div>
       <Header />
@@ -20,25 +31,20 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title}{...item}/>
+            ))}
           </ul>
         </section>
         <h2>Time to get started!</h2>
         <section id="examples">
           <menu>
-          <BtnComp onSelect={() => onSelectHandler("components")}>Components</BtnComp>
-          <BtnComp onSelect={() => onSelectHandler("jsx")}>JSX</BtnComp>
-          <BtnComp onSelect={() => onSelectHandler("props")}>Props</BtnComp>
-          <BtnComp onSelect={() => onSelectHandler("state")}>States</BtnComp>
+            <BtnComp isSelected = {selectedStateValue=="components"} onSelect={() => onSelectHandler("components")}>Components</BtnComp>
+            <BtnComp isSelected = {selectedStateValue=="jsx"} onSelect={() => onSelectHandler("jsx")}>JSX</BtnComp>
+            <BtnComp isSelected = {selectedStateValue=="props"} onSelect={() => onSelectHandler("props")}>Props</BtnComp>
+            <BtnComp isSelected = {selectedStateValue=="state"} onSelect={() => onSelectHandler("state")}>States</BtnComp>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedStateValue].title}</h3>
-            <p>{EXAMPLES[selectedStateValue].description}</p>
-            <code>{EXAMPLES[selectedStateValue].code}</code>
-          </div>
+         {tabContent}
         </section>
       </main>
     </div>
--- src/Components/BtnComp.jsx
+++ src/Components/BtnComp.jsx
@@ -1,7 +1,7 @@
-export default function BtnComp({children, onSelect}){
+export default function BtnComp({children, onSelect, isSelected}){
     return  (
     <li>
-        <button onClick={onSelect}> 
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}> 
             {children}
             </button>
     </li>
